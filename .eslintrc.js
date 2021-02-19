2c1107f4e7c441c13336949a6ac4e9610c021c79
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-19 21:57:33 +0100

    Completed the back setup.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    parser: 'babel-eslint',
+    env: {
+      browser: true,
+      commonjs: true,
+      es6: true,
+      node: true,
+      jest: true,
+    },
+    parserOptions: {
+      ecmaVersion: 2020,
+      ecmaFeatures: {
+        impliedStrict: true,
+        jsx: true,
+      },
+      sourceType: 'module',
+    },
+    plugins: ['react', 'react-hooks'],
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+    ],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      indent: ["error", 2, { SwitchCase: 1 }],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+      "no-console": [
+          "warn",
+          { allow: ["clear", "info", "error", "dir", "trace"] }
+      ]
+    },
+  };
+  
