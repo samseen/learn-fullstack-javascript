cf88e8ba9a7c9b52962148c1fe6f6a58c081d072
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-20 23:43:08 +0100

    Stopping the project, couldn't set it up properly.

---
+++ server.js
@@ -0,0 +1,19 @@
+import config from './config';
+import fs from 'fs';
+
+import express from 'express';
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello express');
+});
+
+server.get('/about.html', (req, res) => {
+    fs.readFile('./about.html', (err, data) => {
+        res.send(data.toString());
+    });
+});
+
+server.listen(config.port, () => {
+    console.info('Express listening on port ', config.port);
+})
